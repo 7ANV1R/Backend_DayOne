ff9be496c3159d61d00e122f2e712dcc96b95a04
7anv1r <ibnmizan7878@gmail.com>
2022-01-18 18:30:32 +0600

    added config

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from "express";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 // app config
 
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url =
+  "mongodb+srv://admin:admin@cluster0.hhix2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // Middlewares
 
 //DB config
 
 // API Endpoints
 
+app.get("/", (req, res) => res.status(200).send("Working!"));
+
 // Listener
+
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
