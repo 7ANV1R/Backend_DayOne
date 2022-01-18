aab9afd5bf747dd07a5c1e48d2351550f07ab5f0
Tanvir <ibnmizan7878@gmail.com>
2022-01-19 03:30:58 +0600

    started on heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
 
 // app config
 
@@ -9,11 +11,41 @@ const connection_url =
   "mongodb+srv://admin:admin@cluster0.hhix2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // Middlewares
 
+app.use(express.json());
+app.use(Cors());
+
 //DB config
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+
+  useUnifiedTopology: true,
+});
 
 // API Endpoints
 
 app.get("/", (req, res) => res.status(200).send("Working!"));
+
+app.post("/tinder/card", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/card", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 // Listener
 
